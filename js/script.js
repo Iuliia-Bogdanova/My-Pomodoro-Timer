5b3b368c32df6be573a04e910658fc5b9ed59edc
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-06-18 16:11:52 +0400

    main done

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
+// declare a constant and assign it an element with the corresponding selector
 const timer = document.querySelector('.timer');
 const title = document.querySelector(".title");
-const start = document.querySelector(".start");
-const pause = document.querySelector(".pause");
-const resume = document.querySelector(".resume");
-const reset = document.querySelector(".reset");
+const startBtn = document.querySelector(".startBtn");
+const pauseBtn = document.querySelector(".pauseBtn");
+const resumeBtn = document.querySelector(".resumeBtn");
+const resetBtn = document.querySelector(".resetBtn");
 const pomoCountsDisplay = document.querySelector(".pomoCountsDisplay");
 
+// Making Variables
 const WORK_TIME = 0.2 * 60;
 const BREAK_TIME = 0.1 * 60;
 let timerID = null;
 let oneRoundCompleted = false;
 let totalCount = 0;
+let paused = false;
 
+// Function to update title
 const updateTitle = (msg) => {
     title.textContent = msg;
 }
 
+// Function to Save pomodoro counts to local storage
 const saveLocalCounts = () => {
     let counts = JSON.parse(localStorage.getItem('pomoCounts'));
     counts !== null ? counts++ : counts = 1;
     localStorage.setItem('pomoCounts', JSON.stringify(counts));
 }
 
+// Function to countdown
 const countDown = (time) => {
     return () => {
         const mins = Math.floor(time / 60).toString().padStart(2, '0');
@@ -47,6 +53,7 @@ const countDown = (time) => {
     }
 }
 
+// Arrow Function to start timer
 const startTimer = (startTime) => {
     if(timerID !== null) {
         stopTimer();
@@ -54,30 +61,55 @@ const startTimer = (startTime) => {
     return setInterval(countDown(startTime), 1000);
 }
 
+// Arrow Function to stop timer
 const stopTimer = () => {
     clearInterval(timerID);
     timerID = null;
 }
 
-start.addEventListener('click', () => {
+// Function to get time in seconds
+const getTimeInSeconds = (timeString) => {
+    const[minutes, seconds] = timeString.split(":");
+    return parseInt(minutes * 60) + parseInt(seconds);
+}
+
+// Adding Event Listener to start button
+startBtn.addEventListener('click', () => {
     timerID = startTimer(WORK_TIME);
     updateTitle("It's Work Time!");
 });
 
-reset.addEventListener('click', () => {
+// Adding Event Listener to reset button
+resetBtn.addEventListener('click', () => {
     stopTimer();
     timer.textContent = "25:00";
+    updateTitle("Click start to start timer");
 });
 
-pause.addEventListener("click", () => {
+// Adding Event Listener to pause button
+pauseBtn.addEventListener("click", () => {
     stopTimer();
-    // timer.textContent = "25:00";
+    paused = true;
+    updateTitle("Timer Paused");
 });
 
+// Adding Event Listener to resume button
+resumeBtn.addEventListener("click", () => {
+    if(paused) {
+        const currentTime = getTimeInSeconds(timer.textContent);
+        timerID = startTimer(currentTime);
+        paused = false;
+        (!oneRoundCompleted) ? updateTitle("It's Work Time") : updateTitle("It's Break Time");
+    }
+});
+
+// Function to show completed pomodoros to screen from local storage
 const showPomoCounts = () => {
     const counts = JSON.parse(localStorage.getItem('pomoCounts'));
-    if(counts > 0){
+    if(counts > 0) {
         pomoCountsDisplay.style.display = "flex";
     }
     pomoCountsDisplay.firstElementChild.textContent = counts;
 }
+
+showPomoCounts();
